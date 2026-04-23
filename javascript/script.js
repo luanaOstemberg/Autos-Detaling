fb2371a494e3b386e31c4f6b04aef538c1a40f23
Usuario <javi.bellido25@gmail.com>
2026-04-23 02:53:44 -0300

    script v2

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function mostrarContacto() {
+    alert("Para solicitar este servicio, por favor contactanos a través de nuestro formulario o redes sociales. ¡Estamos para ayudarte a cuidar tu auto!");
+}
+
+function enviarContacto() {
+    alert("¡Gracias por contactarnos! Hemos recibido tu mensaje y nos pondremos en contacto contigo a la brevedad.");
+}
